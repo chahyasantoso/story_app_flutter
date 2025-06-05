2f2a6fa1958c0c4f00f49f4a88024230784ef0e2
chahyasantoso <chahya.santoso@gmail.com>
2025-06-05 20:42:25 +0800

    Built web
    Branch: main
    Commit: f1b137e1e88c1b4857c5d764218e1ac7b61538ed (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2025465902"
+    serviceWorkerVersion: "1482189545"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -304,7 +304,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "25c269e4a8e022ce1426e4ed45452e85
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "855c42dea1019ac6e2234e147e311337",
+"flutter_bootstrap.js": "851495104b901af127d9292db4786293",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
